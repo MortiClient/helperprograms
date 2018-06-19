4b99906a57ae9ea1d372a67a7e9a32a47b7a80cb
mortim59 <karim_aoulad@outlook.fr>
2018-06-19 17:11:19 +0200

    modified 2 files

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
 	<title>HelperPrograms - Aideur de Programmes C# et C++</title>
 	<link rel="stylesheet" type="text/css" href="stylepage.css">
 	<link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
 </head>
 <body>
 
-	<div class="container">
+	<div class="container" style="margin-top: 35px">
 
 	<h1 class="title-presentation">Bienvenue dans HelperPrograms</h1>
 
@@ -18,8 +19,15 @@
 
 	<h2 class="questions">1/ C'est quoi HelperPrograms ?</h2>
 
-	<div style="background: #9e9e9e;padding: 5px" class="container">
-		<p style="color: #fff;"><i class="fas fa-terminal"></i> C'est un site qui propose des services d'aide sur des programmes C# et VB.NET</p>
+	<div class="content-questions">
+		<p style="color: #fff;">
+			
+			<i class="fas fa-terminal" style="font-size: 20px"></i> 
+			
+			<span style="font-family: 'Raleway', sans-serif;font-size: 18px;">
+				C'est un site qui propose des services d'aide sur des programmes C# et VB.NET
+			</span>
+		</p>
 	</div>
 
 
@@ -27,8 +35,11 @@
 
 	<h2 class="questions">2/ Comment ca marche ?</h2>
 
-	<div style="background: #9e9e9e;padding: 5px" class="container">
-		<p style="color: #fff;"><i class="fas fa-terminal"></i> L'aide se fera par <a href="https://anydesk.fr/plate-formes/windows" style="text-decoration: none;color: blue;">AnyDesk</a>, un logiciel qui vous permettera de travailler en bureau a distance, le service est gratuit, ce dernier vous permettera de vous améliorer dans ces deux langages, et de pouvoir developper des outils sympas. <br> <i>Pour les interessés vous pouvez acheter mes tools , programmes sur ma boutique en ligne...</i><br><a href="https://rocketr.net/sellers/ByRxModzZ" class="btn btn-success" style="margin-top: 5px">Accèder a la boutique</a></p>
+	<div class="content-questions">
+		<p style="color: #fff;font-family: 'Raleway', sans-serif;font-size: 18px;"><i class="fas fa-terminal"></i>
+		 L'aide se fera par <a href="https://anydesk.fr/plate-formes/windows" style="text-decoration: none;color: blue;">AnyDesk</a>, un logiciel qui vous permettera de travailler en bureau a distance, le service est gratuit, ce dernier vous permettera de vous améliorer dans ces deux langages, et de pouvoir developper des outils sympas. <br> <i>Pour les interessés vous pouvez acheter mes tools , programmes sur ma boutique en ligne...</i>
+		 <br>
+		 <a href="https://rocketr.net/sellers/ByRxModzZ" class="btn btn-success" style="margin-top: 5px">Accèder a la boutique</a></p>
 	</div>
 
 
@@ -36,8 +47,8 @@
 
 	<h2 class="questions">3/ Me contacter ?</h2>
 
-	<div style="background: #9e9e9e;padding: 5px" class="container">
-		<p style="color: #fff;"><i class="fas fa-terminal"></i> Vous pouvez me contacter sur Discord, sur ce <a href="https://discord.gg/dYVNuRy" style="text-decoration: none;color: blue;">serveur</a> ou en pv sur ce compte: <b>魚 ЪÿɌxппøÐzŽ 魚#9961</b></p>
+	<div class="content-questions">
+		<p style="color: #fff;font-family: 'Raleway', sans-serif;"><i class="fas fa-terminal"></i> Vous pouvez me contacter sur Discord, sur ce <a href="https://discord.gg/dYVNuRy" style="text-decoration: none;color: blue;">serveur</a> ou en pv sur ce compte: <b>魚 ЪÿɌxппøÐzŽ 魚#9961</b></p>
 	</div>
 
 
